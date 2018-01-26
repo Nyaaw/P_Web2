94c6c3842dfe07ec94d8055cdc8ea77946310533
Loic <loicrossetbv@gmail.com>
2018-01-26 09:23:15 +0100

    ajout du plugin carousel

---
+++ Accueil.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" href="./assets/img/favicon.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>Now Ui Kit by Creative Tim</title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+    <!-- CSS Files -->
+    <link href="./assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="./assets/css/now-ui-kit.css?v=1.1.0" rel="stylesheet" />
+    <!-- CSS Just for demo purpose, don't include it in your project -->
+    <link href="./assets/css/demo.css" rel="stylesheet" />
+</head>
+
+<?php/**
+ * ETML
+ * Auteur: rossetlo
+ * Date: 26.01.2018
+ * Description:
+ */?>
+
+    <body>
+        <div class="wrapper">
+            <div class="page-header clear-filter">
+                <div class="page-header-image" data-parallax="true" style="background-image: url('./assets/img/img_fond_noir.jpg');">
+                </div>
+                <div class="container">
+                    <div class="content-center brand">
+                        <img class="n-logo" src="./assets/img/now-logo.png" alt=\"\">
+                        <h1 class="h1-seo">Passion lecture</h1>
+                        <h3>Un site pour les fans de littératures en tout genre et rédacteurs à mi-temps.</h3>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="span12">
+                            <div class="well">
+                                <div id="myCarousel" class="carousel fdi-Carousel slide">
+                                    <!-- Carousel items -->
+                                    <div class="carousel fdi-Carousel slide" id="eventCarousel" data-interval="0">
+                                        <div class="carousel-inner onebyone-carosel">
+                                            <div class="item active">
+                                                <div class="col-md-4">
+                                                    <a href="#"><img src="http://placehold.it/250x250" class="img-responsive center-block"></a>
+                                                    <div class="text-center">1</div>
+                                                </div>
+                                            </div>
+                                            <div class="item">
+                                                <div class="col-md-4">
+                                                    <a href="#"><img src="http://placehold.it/250x250" class="img-responsive center-block"></a>
+                                                    <div class="text-center">2</div>
+                                                </div>
+                                            </div>
+                                            <div class="item">
+                                                <div class="col-md-4">
+                                                    <a href="#"><img src="http://placehold.it/250x250" class="img-responsive center-block"></a>
+                                                    <div class="text-center">3</div>
+                                                </div>
+                                            </div>
+                                            <div class="item">
+                                                <div class="col-md-4">
+                                                    <a href="#"><img src="http://placehold.it/250x250" class="img-responsive center-block"></a>
+                                                    <div class="text-center">4</div>
+                                                </div>
+                                            </div>
+                                            <div class="item">
+                                                <div class="col-md-4">
+                                                    <a href="#"><img src="http://placehold.it/250x250" class="img-responsive center-block"></a>
+                                                    <div class="text-center">5</div>
+                                                </div>
+                                            </div>
+                                            <div class="item">
+                                                <div class="col-md-4">
+                                                    <a href="#"><img src="http://placehold.it/250x250" class="img-responsive center-block"></a>
+                                                    <div class="text-center">6</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <a class="left carousel-control" href="#eventCarousel" data-slide="prev"></a>
+                                        <a class="right carousel-control" href="#eventCarousel" data-slide="next"></a>
+                                    </div>
+                                    <!--/carousel-inner-->
+                                </div><!--/myCarousel-->
+                            </div><!--/well-->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+
+
+</html>
