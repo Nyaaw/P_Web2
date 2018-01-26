ed4c426826cb43a340a5bdddc62a77537d6ae5ae
Loic <loicrossetbv@gmail.com>
2018-01-26 09:57:48 +0100

    modification du script

--- script.php
+++ script.php
@@ -1,33 +1,35 @@
 <?php
-/*
- * ETML
- * Auteur : Pierric Ripoll
- * Date: 26.01.2018
- * Description : fichier qui sera include dans les pages du site
+/**
+ * Created by PhpStorm.
+ * User: rossetlo
+ * Date: 19.01.2018
+ * Time: 10:45
  */
 ?>
 <!--   Core JS Files   -->
-<script src="./assets/js/core/jquery.3.2.1.min.js" type="text/javascript"></script>
+<script src="./jquery-3.2.1.js" ></script>
 <script src="./assets/js/core/popper.min.js" type="text/javascript"></script>
 <script src="./assets/js/core/bootstrap.min.js" type="text/javascript"></script>
 <!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
-    <script src="./assets/js/plugins/bootstrap-switch.js"></script>
+<script src="./assets/js/plugins/bootstrap-switch.js"></script>
 <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
 <script src="./assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
 <!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
 <script src="./assets/js/plugins/bootstrap-datepicker.js" type="text/javascript"></script>
 <!-- Control Center for Now Ui Kit: parallax effects, scripts for the example pages etc -->
 <script src="./assets/js/now-ui-kit.js?v=1.1.0" type="text/javascript"></script>
+<!-- Plugin de Carousel créé par pradeep330 : https://bootsnipp.com/snippets/featured/thumbnail-carousel-single-image-sliding -->
+<script src="assets/js/plugins/carousel.js" type="text/javascript"></script>
 <script type="text/javascript">
-            $(document).ready(function() {
-                // the body of this function is in assets/js/now-ui-kit.js
-                nowuiKit.initSliders();
-            });
+    $(document).ready(function() {
+        // the body of this function is in assets/js/now-ui-kit.js
+        nowuiKit.initSliders();
+    });
 
     function scrollToDownload() {
 
         if ($('.section-download').length != 0) {
-            $("html, body").animate({
+            $(\"html, body\").animate({
                 scrollTop: $('.section-download').offset().top
             }, 1000);
         }
